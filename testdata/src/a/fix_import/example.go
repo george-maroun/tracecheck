73e9c070417bdd58acfd9d782a43d3ba7ff4d5d9
George M <gsmaroun@gmail.com>
2023-08-11 12:18:07 -0400

    WIP: fixing bug

--- testdata/src/a/fix_import/example.go
+++ testdata/src/a/fix_import/example.go
@@ -13,7 +13,15 @@ func SomeFunc(ctx context.Context, eventType, deliveryID string, payload []byte)
 	return nil
 }
 
-func SomeFunc1(eventType, deliveryID string, payload []byte) error {
+func SomeFunc1(ctx context.Context, eventType, deliveryID string, payload []byte) error {
+	const traceLogKey = "dummyTraceId"
+	log := zapr.NewLogger(zap.L()).WithValues(traceLogKey, "someValue") // cannot be detected
+	log = log.WithValues("eventType", "hello")
+	log.Info("Tracing")
+	return nil
+}
+
+func SomeFunc2(eventType, deliveryID string, payload []byte) error {
 	log := zapr.NewLogger(zap.L()).WithValues("eventType", eventType, "deliverID", deliveryID) // cannot be detected
 	log = log.WithValues("eventType", "hello")
 	log.Info("Tracing")
